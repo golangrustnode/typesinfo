53322e36ae996c3f81d6ac4e27672b3c41cfc630
golangrustnode <hacklinux.org@gmail.com>
2022-07-04 10:59:13 +0800

    initial

---
+++ src/lib.rs
@@ -0,0 +1,26 @@
+//use log::info;
+
+pub mod type_print{
+    use log::info;
+    pub fn print_type_of<T>(_: &T) {
+        info!("concrete-type:{}", std::any::type_name::<T>());
+    }
+}
+
+
+
+
+
+
+#[cfg(test)]
+mod tests {
+    //use crate::type_print::print_type_of;
+    use crate::type_print::print_type_of;
+    #[test]
+    fn print_type(){
+        let x = 123;
+        print_type_of(&x);
+    }
+}
+
+
