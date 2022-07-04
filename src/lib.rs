fdbbf91a3b93646448bff9e551fb28d1ad7c0333
golangrustnode <hacklinux.org@gmail.com>
2022-07-04 11:21:24 +0800

    add println! to print type info

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ pub mod type_print{
     use log::info;
     pub fn print_type_of<T>(_: &T) {
         info!("concrete-type:{}", std::any::type_name::<T>());
+        println!("concrete-type:{}", std::any::type_name::<T>());
     }
 }
 
